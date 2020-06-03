3d31287ce54bde4ed19817c6526bb463519adc53
Eric Osborne <eric@notcom.com>
2020-06-03 06:15:11 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+`vsleep` is a small program to provide a progress indicator and sleep.
+I like it for long-running sleeps.
+Usage: vsleep <time>, where time is in seconds, accurate to one decimal
+point.
+
+`vsleep 3.8`: sleep for 3.8 seconds
+`vsleep 2` or `vsleep 2.0`: sleep for 2 seconds
+
+Minimum sleep interval is 100ms.
+
---
+++ vsleep.go
@@ -0,0 +1,51 @@
+package main
+
+/*
+take arg for number of seconds, either int or float of up to 3 digits precision
+echo stdin to stdout after sleep
+*/
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/sbwhitecap/tqdm"
+	"github.com/sbwhitecap/tqdm/iterators"
+)
+
+func main() {
+
+	// take vsleep as a float, x.y, 1-digit
+	// multiply by 10 to get xy
+	// xy is the integer number of 100ms sleep intervals
+
+	if len(os.Args) == 1 {
+		fmt.Println("usage: vsleep <units>, where <units> are seconds. Accurate to one decimal place.")
+	}
+
+	if len(os.Args) != 2 {
+		return
+	}
+
+	sleepTime, err := strconv.ParseFloat(os.Args[1], 32) // x.y seconds
+	if err != nil {
+		return
+	}
+
+	sleepCount := math.Round(sleepTime * 10)
+	//fmt.Println("sleepTime", sleepTime, "sleepCount", sleepCount)
+
+	if err == nil {
+		tqdm.With(iterators.Interval(0, int(sleepCount)), "", func(v interface{}) (brk bool) {
+			time.Sleep(100 * time.Millisecond)
+			return
+		})
+
+	} else {
+		fmt.Println("ERROR", err)
+	}
+	return
+}
